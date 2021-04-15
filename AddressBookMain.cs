e157137cb5a5884bd3b2c93eaf1a9f401ff23d50
ok97 <omkhawshi0@gmail.com>
2021-04-15 22:05:40 +0530

    Added: Contact Details

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 
 namespace Address_Book_System
-{
+{    /* UC1:- Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and
+              email...
+              - Program is written using IDE like IntelliJ
+              - Every UC is in a separate Git Branch and then merged with main
+              - Naming Convention, Indentation, etc Code Hygiene will be checked during Review
+              - Git Check In Comments and Version History will be monitored
+    */
     class AddressBookMain
     {
         static void Main(string[] args)
