21e0138570e834d379b3fde17545b097df6ad7a9
ok97 <omkhawshi0@gmail.com>
2021-04-15 22:00:33 +0530

    Added: Contact Details

---
+++ AddressBookMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Address_Book_System
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******* Welcome To AddressBook *******");
+          
+            Console.Write("Enter First Name:- ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter Last Name:- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address:- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City:- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State:- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip Code :- ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number:- ");
+            string pNumber = Console.ReadLine();
+            Console.Write("Enter Email:- ");
+            string Email = Console.ReadLine();
+        }
+    }
+}
